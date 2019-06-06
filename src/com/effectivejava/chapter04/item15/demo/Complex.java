dc4b9c4853a1c718393e4adc4dc5daba48c30482
lobef <thankamt@163.com>
2019-06-06 23:30:41 +0800

    学习使可变性最小化

---
+++ src/com/effectivejava/chapter04/item15/demo/Complex.java
@@ -0,0 +1,66 @@
+package com.effectivejava.chapter04.item15.demo;
+
+// 被final修饰的类不能被继承
+// 不可变对象比较简单
+public final class Complex {
+
+	public static final Complex ZERO = new Complex(0, 0);
+	public static final Complex ONE = new Complex(1, 0);
+	public static final Complex I = new Complex(0, 1);
+
+	private final double re;
+	private final double im;
+
+	// 通过构造函数赋值
+	public Complex(double re, double im) {
+		this.re = re;
+		this.im = im;
+	}
+
+	// --获取相应的数值---
+	public double realPart() {
+		return re;
+	}
+
+	public double imaginaryPart() {
+		return im;
+	}
+
+	// 四则运算法则 ：加减乘除
+	public Complex plus(Complex c) {
+		return new Complex(re + c.re, c.im + im);
+	}
+
+	public Complex minus(Complex c) {
+		return new Complex(re - c.re, im - c.im);
+	}
+
+	public Complex times(Complex c) {
+		return new Complex(re * c.re - im * c.im, re * c.im - im * c.re);
+	}
+
+	public Complex dividedBy(Complex c) {
+		double tmp = c.re * c.re + c.im * im;
+		return new Complex((re * c.re + im * c.im) / tmp, (im * c.re - re * c.im) / tmp);
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (o == this)
+			return true;
+		if (!(o instanceof Complex))
+			return false;
+		Complex c = (Complex) o;
+		return Double.compare(re, c.re) == 0 && Double.compare(im, c.im) == 0;
+	}
+
+	@Override
+	public int hashCode() {
+		return 31 * Double.hashCode(re) + Double.hashCode(im);
+	}
+
+	public String toString() {
+		return "(" + re + " + " + im + "i)";
+	}
+
+}
---
+++ src/com/effectivejava/chapter04/item15/demo/ImmutableObject.java
@@ -0,0 +1,17 @@
+package com.effectivejava.chapter04.item15.demo;
+
+import java.math.BigInteger;
+import java.util.BitSet;
+
+// 不可变对象的缺点演示
+// 重复创建对象，对性能有一定影响
+public class ImmutableObject {
+
+	public static void main(String[] args) {
+
+		BigInteger moby = new BigInteger("1222");
+		moby = moby.flipBit(0);
+		BitSet moby1 = new BitSet(2343434);
+		moby1.flip(0);
+	}
+}
